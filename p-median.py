78e28760f91bc6df36eb567444d51cf6019cb497
Emanuele Di Fiore <manu.difiore@gmail.com>
2019-11-09 17:43:20 +0100

    UPDATE DAY 2

--- p-median.py
+++ p-median.py
@@ -2,6 +2,7 @@ from scipy.io import arff
 from scipy.spatial.distance import pdist, squareform
 import pandas as pd
 import numpy
+import random
 
 # Lettura dati dal file arff
 def read_file(arff_file):
@@ -16,42 +17,80 @@ def distance_matrix(df, nome_file):
     dist_matrix = squareform(distances)
 
     # Salvataggio su file
-    numpy.save(nome_file, dist_matrix)
+    #numpy.save(nome_file, dist_matrix)
     return dist_matrix
 
-def funzione_obiettivo(row, dist_matrix, index_centroids):
+def p_median(p, df, dist_matrix):
+    return 0
+
+def funzione_obiettivo(dist_matrix, index_centroids):
 
     minimi = []
-    for r in range(0,len(dist_matrix)+1):
+    for r in range(len(dist_matrix)):
         if not(r in index_centroids):
             distances = []
             for c in index_centroids:
-                distances.append(dist_matrix[r,c])
-                #print(dist_matrix[r,c])
+                distances.append(dist_matrix[r][c])
+                
             minimi.append(min(distances))
-            #print(minimi)
 
     somma = sum(minimi)
-    #print(somma)
+
     return somma
 
-def p_median(p, df, dist_matrix):
-    row = df.sample(n=p)
-    dirty_row = row
-    index_centroids = list(numpy.array(row['a0'].index))
-    temp_row = pd.concat([row, df]).drop_duplicates(keep=False)
+def p_median(p, dist_matrix):
+    # indici dei centroidi iniziali
+    #median = random.sample(range(len(dist_matrix)), p)
+    median = [0, 1, 2]
 
-    print(row)
-    for index, r in temp_row.iterrows():
-        print(row.drop(index))
+    z = funzione_obiettivo(dist_matrix, median)
+    finito = False
+    old_best_j = -1
+    old_best_i = -1
 
-    #z_iniziale = funzione_obiettivo(row, dist_matrix, index_centroids)
+    while not(finito):
+
+        print("Inizio iterazione")
+        best_saving = 0
+        best_j = 0
+        best_i = 0
+        best_f_ob = 0
+        s_ij = 0
+
+        print(median)
+        for i in range(len(dist_matrix)):
+            if i not in median:
+                for j in range(len(median)):
+                    #if(i != old_best_j and median[j] != old_best_i):
+                    dirty_median = median.copy()
+                    print(str(i) + " " + str(j))
+                    print("Valutazione sostituzione " + str(i) + " " + str(median[j]))
+                    dirty_median[j] = i
+                    f_ob = funzione_obiettivo(dist_matrix, dirty_median)
+                    s_ij = z - f_ob
+
+                    if s_ij > best_saving:
+                        best_saving = s_ij
+                        best_i = i
+                        best_j = j
+                        best_f_ob = f_ob
+
+        if best_saving > 0:
+            #old_best_j = median[best_j]
+            #old_best_i = best_i
+            median[best_j] = best_i
+            z = best_f_ob
+            print("Best f-ob: " + str(best_f_ob))
+            print("Best i, best j: " + str(best_i) + " " + str(best_j))
+        else:
+            finito = True
+
     return 0
 
-df = read_file('./datasets/2d-10c.arff')
-dist_matrix = distance_matrix(df, 'distance_matrix')
-p = 5
-p_median(p, df, dist_matrix)
-
-
-
+if __name__ == "__main__":
+    #df = read_file('./datasets/2d-10c.arff')
+    #dist_matrix = distance_matrix(df, 'distance_matrix')
+    #data = df.values
+    p = 3
+    dist_matrix = [[0, 5, 7, 3, 4, 9], [5, 0, 10, 6, 8, 5], [7, 10, 0, 4, 7, 8], [3, 6, 4, 0, 2, 9], [4, 8, 7, 2, 0, 6], [9, 5, 8, 9, 6, 0]]
+    p_median(p, dist_matrix)
